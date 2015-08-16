5fc29f9c5e256a76412745c08267f76159e6c63b
Xin Li <lixin.reco@gmail.com>
2015-08-16 15:06:27 +0200

    added copyright info in *.c and *.h files as header comments

--- basis.h
+++ basis.h
@@ -1,3 +1,22 @@
+/*****************************************************************************
+ This file is part of the XLQC program.                                      
+ Copyright (C) 2015 Xin Li <lixin.reco@gmail.com>                            
+                                                                           
+ Filename:  basis.h                                                      
+ License:   BSD 3-Clause License
+
+ This software is provided by the copyright holders and contributors "as is"
+ and any express or implied warranties, including, but not limited to, the
+ implied warranties of merchantability and fitness for a particular purpose are
+ disclaimed. In no event shall the copyright holder or contributors be liable
+ for any direct, indirect, incidental, special, exemplary, or consequential
+ damages (including, but not limited to, procurement of substitute goods or
+ services; loss of use, data, or profits; or business interruption) however
+ caused and on any theory of liability, whether in contract, strict liability,
+ or tort (including negligence or otherwise) arising in any way out of the use
+ of this software, even if advised of the possibility of such damage.
+ *****************************************************************************/
+
 // allocate memory with failure checking
 void* my_malloc(size_t bytes);
 
